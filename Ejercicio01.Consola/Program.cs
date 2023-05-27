badd028fab0632e8ab0b36896b20c0622dd596e8
BiancaLopresto <bianlopre19@gmail.com>
2023-05-27 00:53:05 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio01.Consola/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numero1;
+int numero2;
+int suma;
+Console.Write("Ingrese el primer numero a sumar:");
+numero1 = int.Parse(Console.ReadLine());
+Console.Write("Ingrese el segundo numero a sumar:");
+numero2= int.Parse(Console.ReadLine());
+suma = numero1 + numero2;
+ Console.WriteLine($"La suma es: {suma}");
+
+
